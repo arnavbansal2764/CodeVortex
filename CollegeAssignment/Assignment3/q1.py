4a77f47b9ffc6f71483fdbd50803272f0bdb65ff
Arnav Bansal <70932975+arnavbansal2764@users.noreply.github.com>
2023-09-30 22:37:12 +0530

    Update q1.py

--- CollegeAssignment/Assignment3/q1.py
+++ CollegeAssignment/Assignment3/q1.py
@@ -1,16 +1,13 @@
-#print armstrong numbers between 1 to 500
-def is_armstrong_number(number):
-    # Convert the number to a string to count its digits
-    num_str = str(number)
-    num_digits = len(num_str)
-    
-    # Calculate the sum of each digit raised to the power of the number of digits
-    armstrong_sum = sum(int(digit) ** num_digits for digit in num_str)
-    
-    # Check if the sum is equal to the original number
-    return armstrong_sum == number
+def armstrong(x):
+    a = str(x)
+    sum = 0
+    for i in range(0,len(a)):
+        sum = sum + (int(a[i])**3)
+    if(sum == x):
+        return True
+    else:
+        return False
 
-# Iterate through numbers from 1 to 500 and print Armstrong numbers
-for num in range(1, 501):
-    if is_armstrong_number(num):
-        print(num)
+for a in range(1,501):
+    if(armstrong(a)):
+        print(a)
